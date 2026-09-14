b03df09dc3f205755fa290a6925ec4e0b6266df5
Aditya Patsariya <patsariyaaditya91@gmail.com>
2026-09-14 17:34:59 +0530

    implemented prime and vowel question

---
+++ basic_question/prime.py
@@ -0,0 +1,13 @@
+def isPrime(n):
+    if n <= 1 :
+       print("not a prime number")
+
+    else:
+      for i in range(2,n):
+        if n%i == 0:
+           print("not a prime number")
+           break
+      else:
+        print("is a prime number")
+
+isPrime(13)
---
+++ basic_question/vowel.py
@@ -0,0 +1,8 @@
+def isVowel(ch):
+    
+    if ch in "aeiouAEIOU":
+        print("vowel is present")
+    else:
+        print("consonant is present")
+
+isVowel("b")            
